20852aae83a099f86a91747b8726138f3369c59b
Sergio Gómez <hola@sergio-gomez.com>
2016-01-26 17:09:38 +0100

    Annotating emotion and user models

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: emotions
+#
+#  id         :integer          not null, primary key
+#  status     :integer
+#  user_id    :integer
+#  emotion_on :date             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Emotion < ActiveRecord::Base
   belongs_to :user, inverse_of: :emotions
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
   has_many :emotions, inverse_of: :user, dependent: :destroy
 end
